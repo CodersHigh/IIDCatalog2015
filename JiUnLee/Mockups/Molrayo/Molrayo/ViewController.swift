72515454ad0097c7c08ef555f566590e935083cc
jlee369 <jiun.lee95@gmail.com>
2015-11-24 18:23:24 +0900

    Create New Molrayo

---
+++ JiUnLee/Mockups/Molrayo/Molrayo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Molrayo
+//
+//  Created by Ji Un Lee on 11/24/15.
+//  Copyright © 2015 Ji Un Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
